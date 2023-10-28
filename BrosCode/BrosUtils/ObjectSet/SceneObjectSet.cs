aef95b7254a576f7a468cfee90784da71ed8d75a
foreyes <foreyes_1002@163.com>
2023-10-27 23:28:56 -0400

    添加功能：ObjectSet，AsyncHelper

---
+++ BrosCode/BrosUtils/ObjectSet/SceneObjectSet.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.Reflection;
+
+public class SceneObjectSet : MonoBehaviour
+{
+    [SerializeField]
+    private List<ManagedItem<Object>> managedItems = new List<ManagedItem<Object>>();
+
+    void OnValidate() {
+        // Check for duplicate names
+        var duplicateNames = managedItems.GroupBy(x => x.name).Where(g => g.Count() > 1).Select(y => y.Key).ToList();
+        if (duplicateNames.Count > 0) {
+            Debug.LogError($"SceneObjectSet: Duplicate names found - {string.Join(", ", duplicateNames)}");
+        }
+
+        // Retrieve the objectName value, whether it's a property or a field
+        for (int i = 0; i < managedItems.Count; i++) {
+            if (string.IsNullOrEmpty(managedItems[i].name) && managedItems[i].item != null) {
+                var type = managedItems[i].item.GetType();
+
+                // Get the value of objectName, either from a property or a field
+                object objectNameValue = type.GetProperty("objectName", BindingFlags.Public | BindingFlags.Instance)?.GetValue(managedItems[i].item)
+                                    ?? type.GetField("objectName", BindingFlags.Public | BindingFlags.Instance)?.GetValue(managedItems[i].item);
+
+                if (objectNameValue != null) {
+                    managedItems[i] = new ManagedItem<Object>(objectNameValue.ToString(), managedItems[i].item);
+                }
+            }
+        }
+    }
+
+    public T GetObject<T>(string name) where T : Object {
+        var foundItem = managedItems.FirstOrDefault(x => x.name == name && x.item is T);
+        if (foundItem.item != null) {
+            return foundItem.item as T;
+        }
+        return null;
+    }
+}
